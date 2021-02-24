633fd0f5dc42b62272a3b71d012a7cedba83f013
duraes-antonio <garotoseis@gmail.com>
2021-02-24 07:36:39 -0300

    FIX?: Tela de merge trava execução e não dá pra confirmar commit em plataformas online (kaggle)

--- src/execution/helper/git.py
+++ src/execution/helper/git.py
@@ -16,10 +16,11 @@ class Git:
 	def config_user(self):
 		os.system(f"""git config --global user.email "{self.user_email}" """)
 		os.system(f"""git config --global user.name "{self.user}" """)
+		os.system("""git config --global core.mergeoptions --no-edit""")
 
 	def add_commit_item(self, item_local_path: str, commit_msg: str):
 		os.system(f"""git add {item_local_path}""")
-		os.system(f"""git commit -m "{commit_msg}" """)
+		os.system(f"""git commit -m "{commit_msg}" --no-edit """)
 
 	def pull_changes(self):
 		os.system("git pull")
